3d2bdc633fe3882eedbcd82368d04c88aec6fe32
Matt Aaron <13080357+msaaron@users.noreply.github.com>
2022-03-31 09:27:48 -0400

    Initial Commit

---
+++ LabMobileApp/LabMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabMobileAppApp.swift
+//  LabMobileApp
+//
+//  Created by Matt Aaron on 3/31/22.
+//
+
+import SwiftUI
+
+@main
+struct LabMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
